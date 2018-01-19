c31ac9776e81cd3f9a9d94b8fa452ed7c329a8a5
Egor <beast.loby@gmail.com>
2018-01-19 19:12:47 +0300

    add news-item component

---
+++ src/app/news-item/news-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'news-item',
+  templateUrl: './news-item.component.html',
+  styleUrls: ['./news-item.component.scss']
+})
+export class NewsItemComponent {
+
+}
