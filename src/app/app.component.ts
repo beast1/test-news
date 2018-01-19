c65ad15c9726b2a345d6db78e1d973d8779bd741
Egor <beast.loby@gmail.com>
2018-01-19 20:09:02 +0300

    пеердал в цикл локальную дату

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  newsItemData = [{
+    'title': '1message from parent',
+    'descr': '1secd',
+    'preview': 'http://test.kamis.ru/1.jpg'
+  }, {
+    'title': '2message from parent',
+    'descr': '2secd',
+    'preview': 'http://test.kamis.ru/2.jpg'
+  }, {
+    'title': '3message from parent',
+    'descr': '3secd',
+    'preview': 'http://test.kamis.ru/3.jpg'
+  }];
 }
--- src/app/news-item/news-item.component.ts
+++ src/app/news-item/news-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'news-item',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./news-item.component.scss']
 })
 export class NewsItemComponent {
-
+  @Input() childData: object;
 }
