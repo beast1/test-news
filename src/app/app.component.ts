86899e82a194c32fc6d23c30428c2ffccf96f550
Egor <beast.loby@gmail.com>
2018-01-20 00:39:18 +0300

    почист лишние

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
 
--- src/app/news-item/news-item.component.ts
+++ src/app/news-item/news-item.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'news-item',
-  templateUrl: './news-item.component.html',
-  styleUrls: ['./news-item.component.scss']
+  templateUrl: './news-item.component.html'
 })
 export class NewsItemComponent {
   @Input() childData: object;
